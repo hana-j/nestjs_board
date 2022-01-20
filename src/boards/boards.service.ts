96eb14014aae0cbafac5257b13f446a340355865
hana <aswjdgksk@gmail.con>
2022-01-20 21:41:21 +0900

    boardDto

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
 export class BoardsService {
@@ -10,9 +11,10 @@ export class BoardsService {
     return this.boards;
   }
 
-  createBoard(title: string, description: string) {
+  createBoard(createBoardDto: CreateBoardDto) {
+    const { title, description } = createBoardDto;
     const board: Board = {
-      id: uuid,
+      id: uuid(),
       title,
       description,
       status: BoardStatus.PUBLIC,
