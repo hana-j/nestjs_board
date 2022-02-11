5845a5302a0e3607855b05ecd20d1d59e129c694
hana <aswjdgksk@gmail.con>
2022-02-11 21:29:15 +0900

    bcrypt.js 사용

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,6 +1,14 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+} from 'typeorm';
 
 @Entity()
+@Unique(['username'])
+//Unique하지 않은 값이 들어왔을때 try catch구문이 없다면 그냥 controller레벨로 넘어가 500서버에러를 낸다.
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
