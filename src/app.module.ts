cf70bc9289f551e506f25e9c66418ecb0af23ead
hana <aswjdgksk@gmail.con>
2022-02-18 22:56:47 +0900

    passport-jwt 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,43 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { BoardsModule } from './boards/boards.module';
-import { typeORMConfig } from './configs/typeorm.config';
-import { AuthModule } from './auth/auth.module';
-import { JwtModule } from '@nestjs/jwt';
-import * as Joi from 'joi';
 import { ConfigModule } from '@nestjs/config';
+import { typeORMConfig } from './configs/typeorm.config';
+import { BoardsModule } from './boards/boards.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
-      ignoreEnvFile: process.env.NODE_ENV === 'prod',
-      // validationSchema: Joi.object({
-      //   NODE_ENV: Joi.string().valid('dev', 'prod').required(),
-      //   DB_HOST: Joi.string().required(),
-      //   DB_PORT: Joi.string().required(),
-      //   DB_USERNAME: Joi.string().required(),
-      //   DB_PASSWORD: Joi.string().required(),
-      //   DB_NAME: Joi.string().required(),
-      // }),
-    }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      synchronize: process.env.NODE_ENV !== 'prod',
-      logging: true,
-    }),
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: {
-        expiresIn: 3600,
-      },
-    }),
-  ],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule, AuthModule],
 })
 export class AppModule {}
